95e6e88d25fb4577d05c272b7d3de0c5dbd1d453
Sepras <shirokisaragi32@gmail.com>
2024-10-24 22:23:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import PokemonList from "./components/PokemonList/PokemonList";
+
+function App() {
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
+}
+
+export default App;
